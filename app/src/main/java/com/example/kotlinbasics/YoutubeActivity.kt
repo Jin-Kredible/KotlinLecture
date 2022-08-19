6f8a57b5b1234d92f99781cea166449f934abee3
JINWON KIM <jinwkim90@gmail.com>
2022-08-19 23:05:59 +0900

    -youtube demo

---
+++ app/src/main/java/com/example/kotlinbasics/YoutubeActivity.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinbasics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class YoutubeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube)
+
+
+        val retrofit =
+
+    }
+}
