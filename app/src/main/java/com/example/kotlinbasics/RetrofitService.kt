9dbb71dc22ab0d2e5989ded02bf61b907912e813
JINWON KIM <jinwkim90@gmail.com>
2022-08-19 23:36:01 +0900

    -youtube demo -2

--- app/src/main/java/com/example/kotlinbasics/RetrofitService.kt
+++ app/src/main/java/com/example/kotlinbasics/RetrofitService.kt
@@ -5,9 +5,14 @@ import retrofit2.http.GET
 
 class StudentFromServer( val id: Int, val name: String, val age : Int, val intro : String)
 
+class YoutubeItem(val id : Int, val title : String, val content : String, val video : String, val thumnail : String)
+
 interface RetrofitService {
 
     @GET("json/students")
     fun getStudnetList(): Call<ArrayList<StudentFromServer>>
+
+    @GET("youtube/list")
+    fun getYoutubeItemList() : Call<ArrayList<YoutubeItem>>
 
 }
--- app/src/main/java/com/example/kotlinbasics/YoutubeActivity.kt
+++ app/src/main/java/com/example/kotlinbasics/YoutubeActivity.kt
@@ -1,7 +1,23 @@
 package com.example.kotlinbasics
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
+import org.w3c.dom.Text
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.zip.Inflater
 
 class YoutubeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,7 +25,57 @@ class YoutubeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_youtube)
 
 
-        val retrofit =
+        val retrofit = Retrofit.Builder().baseUrl("http://mellowcode.org/").addConverterFactory(GsonConverterFactory.create()).build()
+        val retrofitService = retrofit.create(RetrofitService::class.java)
 
+        retrofitService.getYoutubeItemList().enqueue(object : Callback<ArrayList<YoutubeItem>>{
+            override fun onResponse(
+                call: Call<ArrayList<YoutubeItem>>,
+                response: Response<ArrayList<YoutubeItem>>
+            ) {
+                val youtubeItemList = response.body()
+
+                youtubeItemList!!.forEach {
+                    Log.d("testt",it.title)
+                }
+            }
+
+            override fun onFailure(call: Call<ArrayList<YoutubeItem>>, t: Throwable) {
+                Log.d("testt","yes")
+            }
+
+        })
+
+    }
+}
+
+class YoutubeListAdapter (val youtubeItemList : ArrayList<YoutubeItem>, val inflater : LayoutInflater, val glide : RequestManager, val context : Context ) : RecyclerView.Adapter<YoutubeListAdapter.ViewHoler> {
+    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val title : TextView
+        val thumbnail : ImageView
+        val content : TextView
+
+        init {
+            title = itemView.findViewById(R.id.title)
+            thumbnail = itemView.findViewById(R.id.thumbnail)
+            content = itemView.findViewById(R.id.content)
+
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): YoutubeListAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun onBindViewHolder(holder: YoutubeListAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return youtubeItemList.size
     }
 }
